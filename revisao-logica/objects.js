74c4f503e5b9ae2c4d019fe729a2062e515588ea
Maguimn <v.eduardo.silva25@gmail.com>
2022-02-22 13:22:28 -0300

    Estudo de arrays

---
+++ revisao-logica/objects.js
@@ -0,0 +1,22 @@
+//const nomes = ["Daniel", "Maria", "Joao"]
+//const idades = [null, 23, 27, 18]
+//console.log(nomes[0], idades[0])
+
+// // const pessoa = new Object()
+// pessoa.nome = "Daniel"
+// pessoa.idade = 40
+// console.log(pessoa)
+// let prop = "nome"
+// console.log(pessoa[prop])
+// console.log(pessoa[idade])
+
+const pessoa = {
+    nome: "Daniel",
+    idade: 40
+}
+pessoa.nome = "Daniel"
+pessoa.idade = 40
+console.log(pessoa)
+let prop = "nome"
+console.log(pessoa[prop])
+console.log(pessoa["idade"])
